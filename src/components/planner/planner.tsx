773da012414a3b81b8def08f350fcca14cb86e61
Oscar <oscartrujillo94@gmail.com>
2020-03-29 13:46:11 +0200

    adding planner

---
+++ src/components/planner/planner.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Planner extends Component {
+  render() {
+    return (
+      <h1>
+        Planner
+      </h1>
+    );
+  }
+}
+ 
+export default Planner;
