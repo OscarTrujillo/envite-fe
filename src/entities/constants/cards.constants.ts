026e33772863726c9a6ef3c05027d4afaed4f3c3
Oscar <oscartrujillo94@gmail.com>
2020-05-16 18:31:14 +0200

    adding new cards

--- src/entities/constants/cards.constants.ts
+++ src/entities/constants/cards.constants.ts
@@ -22,20 +22,64 @@
 
 export const cardsConstants = {
     back: 'back',
-    oro: {
-        1: 'oro_1',
-        11: 'oro_caballo',
+    oros: {
+        1: 'oros_1',
+        2: 'oros_2',
+        3: 'oros_3',
+        4: 'oros_4',
+        5: 'oros_5',
+        6: 'oros_6',
+        7: 'oros_7',
+        8: 'oros_8',
+        9: 'oros_9',
+
+        'sota': 'back',
+        'caballo': 'back',
+        'rey': 'back',
     },
-    copa: {
-        1: 'copa_1',
-        11: 'copa_caballo',
+    copas: {
+        1: 'copas_1',
+        2: 'copas_2',
+        3: 'copas_3',
+        4: 'copas_4',
+        5: 'copas_5',
+        6: 'copas_6',
+        7: 'copas_7',
+        8: 'copas_8',
+        9: 'copas_9',
+
+        'sota': 'back',
+        'caballo': 'back',
+        'rey': 'back',
     },
-    espada: {
-        1: 'espada_1',
-        11: 'espda_caballo',
+    espadas: {
+        1: 'espadas_1',
+        2: 'espadas_2',
+        3: 'espadas_3',
+        4: 'espadas_4',
+        5: 'espadas_5',
+        6: 'espadas_6',
+        7: 'espadas_7',
+        8: 'espadas_8',
+        9: 'espadas_9',
+
+        'sota': 'back',
+        'caballo': 'back',
+        'rey': 'back',
     },
-    basto: {
-        1: 'basto_1',
-        11: 'basto_caballo',
+    bastos: {
+        1: 'bastos_1',
+        2: 'bastos_2',
+        3: 'bastos_3',
+        4: 'bastos_4',
+        5: 'bastos_5',
+        6: 'bastos_6',
+        7: 'bastos_7',
+        8: 'bastos_8',
+        9: 'bastos_9',
+
+        'sota': 'back',
+        'caballo': 'back',
+        'rey': 'back',
     }
 };
